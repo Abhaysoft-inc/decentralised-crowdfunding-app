c368d3f612148fd046dd7eda5c5d613bb3bfa5b9
Abhaysoft-inc <av385735@gmail.com>
2025-04-30 18:45:22 +0530

    added something in cards

--- src/components/CardsComponent.js
+++ src/components/CardsComponent.js
@@ -1,9 +1,10 @@
 import React from 'react'
 
-const CardsComponent = () => {
+const CardsComponent = ({ image }) => {
     return (
         <div>
             <div className="bg-gray-200 h-96 rounded shadow-lg px-2 py-2">
+                <img src={image} className='h-52 w-full object-cover' alt="" />
 
             </div>
         </div>
--- src/components/MainScreen.js
+++ src/components/MainScreen.js
@@ -6,10 +6,10 @@ const MainScreen = () => {
         <>
 
             <div className="grid grid-cols-3 py-3 gap-3 mx-2 space-y-2.5">
-                <CardsComponent />
-                <CardsComponent />
-                <CardsComponent />
-                <CardsComponent />
+                <CardsComponent image={"https://images.unsplash.com/photo-1611270418597-a6c77f4b7271?q=80&w=1396&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"} />
+                <CardsComponent image={"https://plus.unsplash.com/premium_photo-1699562353506-374d31991522?q=80&w=1374&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"} />
+                <CardsComponent image={"https://images.unsplash.com/photo-1743630459079-ff40cb544dc1?q=80&w=1374&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"} />
+                <CardsComponent image={"https://images.unsplash.com/photo-1744375212850-cd9da4c1d24d?q=80&w=1528&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"} />
 
 
 
