76e2c305b6240d949da00a1f436ed8a96f118ff7
Abhaysoft-inc <av385735@gmail.com>
2025-05-04 01:26:37 +0530

    written code for saving the data to appwrite db

---
+++ src/app/api/campaigns/route.js
@@ -0,0 +1,12 @@
+export async function POST(req) {
+    const body = await req.json();
+    const { campaignName, campaignGoal, moreInfo, imageURL } = body;
+
+
+
+
+
+
+
+    console.log("hi")
+}
--- src/app/create-campaign/page.js
+++ src/app/create-campaign/page.js
@@ -1,53 +1,51 @@
 "use client"
+
 import axios from 'axios'
 import { ethers } from 'ethers';
 import Link from 'next/link';
 import React, { useState } from 'react'
 import { IoMdArrowRoundBack } from "react-icons/io";
 import abi from '@/app/ABI.json'
+import Router, { useRouter } from 'next/navigation';
 
 
 const CreateCampaignPage = () => {
     // console.log(abi)
+    const router = useRouter();
 
-    const contractAddress = "0x4Ba6aEAa4CAC4372542aE21E23F98571cD9BA8B4"
+    const contractAddress = "0x4Ba6aEAa4CAC4372542aE21E23F98571cD9BA8B4";
+    const appwrite_api_endpoint = "https://fra.cloud.appwrite.io/v1";
 
 
     const [File, setFile] = useState("");
     const [campaignName, setcampaignName] = useState("")
     const [campaignGoal, setcampaignGoal] = useState("")
-    const [info, setinfo] = useState("")
+    const [info, setinfo] = useState("");
 
-    async function createCampaign() {
-        if (!window.ethereum) return alert("Please install a crypto wallet!");
+    const handleSubmit = async (e) => {
+        e.preventDefault()
+        console.log(File);
+
 
         try {
+            const uniqueId = 'campaign_' + Date.now() + '_' + Math.random().toString(36).substr(2, 9);
+
+
+            // saving data to blockchain
+
             const provider = new ethers.BrowserProvider(window.ethereum);
             const signer = await provider.getSigner();
 
             const contractInstance = new ethers.Contract(contractAddress, abi, signer);
             const newCampaign = await contractInstance.createCampaign(campaignName, campaignGoal);
             const confirmation = await newCampaign.wait();
-            alert("Your campaign is successfully listed");
+            console.log("Your campaign is successfully listed");
             console.log(newCampaign, confirmation);
-            setcampaignGoal("")
-            setcampaignName("")
-            setFile(null)
-            setinfo("")
-
-        } catch (e) {
-            console.log(e)
-
-        }
-    }
 
 
 
-    const handleSubmit = async (e) => {
-        e.preventDefault()
-        console.log(File);
+            // Uploading image to IPFS
 
-        try {
             const data = new FormData();
             data.set("file", File);
 
@@ -60,7 +58,40 @@ const CreateCampaignPage = () => {
                     pinata_secret_api_key: process.env.NEXT_PUBLIC_PINATA_SECRET_KEY,
                 }
             });
-            console.log("https://bronze-payable-wildebeest-639.mypinata.cloud/ipfs/" + response.data.IpfsHash);
+            const imageURL = await "https://bronze-payable-wildebeest-639.mypinata.cloud/ipfs/" + response.data.IpfsHash;
+            console.log(imageURL);
+
+
+            // uploading data to database
+
+            const appwriteResponse = await axios({
+                method: "post",
+                url: "https://fra.cloud.appwrite.io/v1/databases/68166aa50016fc88f31e/collections/68166b3200261be321e5/documents",
+                headers: {
+                    "X-Appwrite-Project": process.env.NEXT_PUBLIC_PROJECT_ID,
+                    "Content-Type": "application/json",
+                    "X-Appwrite-Key": process.env.NEXT_PUBLIC_API_APPWRITE,
+                },
+                data: {
+                    documentId: uniqueId,
+                    data: {
+                        campaignName: campaignName,
+                        campaignGoal: parseInt(campaignGoal),
+                        moreInfo: info,
+                        imageURL: imageURL,
+                        walletAddress: signer.address,
+                        txHash: confirmation.hash,
+
+                    }
+
+                }
+
+            });
+
+            console.log(appwriteResponse);
+            alert("Campaign Creation Successfull!");
+            router.push('/');
+
 
         } catch (error) {
             console.log(error)
@@ -68,9 +99,9 @@ const CreateCampaignPage = () => {
         }
     }
 
-
-
-
+    function goHome() {
+        router.push('/')
+    }
     return (
 
 
@@ -83,7 +114,7 @@ const CreateCampaignPage = () => {
             </div> */}
             <div className="mt-6 mx-10">
                 <Link href={'/'}>
-                    <IoMdArrowRoundBack size={28} className='cursor-pointer' /></Link>
+                    <IoMdArrowRoundBack size={28} className='cursor-pointer' onClick={goHome} /></Link>
 
                 {/* <Navbar /> */}
                 <div className="flex justify-center items-center min-h-screen">
@@ -95,19 +126,19 @@ const CreateCampaignPage = () => {
 
                         <div className="main mt-6">
                             <p className="text-[15px]">Campaign Name</p>
-                            <input type="text" className='border w-full mt-1.5 px-2 py-1 rounded border-gray-300' placeholder='' onChange={(e) => { setcampaignName(e.target.value) }} />
+                            <input type="text" className='border w-full mt-1.5 px-2 py-1 rounded border-gray-300' placeholder='' onChange={(e) => { setcampaignName(e.target.value) }} name='campaignName' />
 
                             <p className="text-[15px] mt-3">Required Amount (USD)</p>
-                            <input type="number" className='border w-full mt-1.5 px-2 py-1 rounded border-gray-300' placeholder='' onChange={(e) => { setcampaignGoal(e.target.value) }} />
+                            <input type="number" className='border w-full mt-1.5 px-2 py-1 rounded border-gray-300' placeholder='' onChange={(e) => { setcampaignGoal(e.target.value) }} name='campaignGoal' />
 
                             <p className="text-[15px] mt-3">More Info</p>
-                            <textarea type="text" className='border w-full mt-1.5 px-2 py-1 rounded border-gray-300' placeholder='' onChange={(e) => { setinfo(e.target.value) }} />
+                            <textarea type="text" className='border w-full mt-1.5 px-2 py-1 rounded border-gray-300' placeholder='' onChange={(e) => { setinfo(e.target.value) }} name='moreInfo' />
 
                             <p className="text-[15px] mt-3">Upload Supporting Documents</p>
 
-                            <input type="file" name="files" id="" className='mt-3 bg-gray-100 w-full px-10 py-2 rounded cursor-pointer' multiple />
+                            <input type="file" name="file" id="" className='mt-3 bg-gray-100 w-full px-10 py-2 rounded cursor-pointer' onChange={(e) => { setFile(e.target.files[0]) }} />
 
-                            <button className='text-center flex justify-center w-full mt-6 bg-blue-600 text-white py-2 rounded cursor-pointer' onClick={createCampaign}>
+                            <button className='text-center flex justify-center w-full mt-6 bg-blue-600 text-white py-2 rounded cursor-pointer' onClick={handleSubmit} >
                                 Create Campaign
 
                             </button>
